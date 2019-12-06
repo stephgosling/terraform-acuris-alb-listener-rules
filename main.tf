9fa58ae8572c49a5aa7c80474559a50a2490a437
Keir Badger <keir.badger@acuris.com>
2019-12-06 13:05:35 +0000

    Upgrade module to Terraform 0.12

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 resource "aws_alb_listener_rule" "rule" {
-  count = "${length(var.path_conditions)}"
+  count = length(var.path_conditions)
 
-  listener_arn = "${var.alb_listener_arn}"
-  priority     = "${var.starting_priority + count.index}"
+  listener_arn = var.alb_listener_arn
+  priority     = var.starting_priority + count.index
 
   action {
     type             = "forward"
-    target_group_arn = "${var.target_group_arn}"
+    target_group_arn = var.target_group_arn
   }
 
   # Always pass host-based routing condition, with '*.*' being default
@@ -14,11 +14,20 @@ resource "aws_alb_listener_rule" "rule" {
   # NOTE: You can have multiple paths but only a single hostname
   condition {
     field  = "host-header"
-    values = ["${var.host_condition}"]
+    values = [var.host_condition]
   }
 
   condition {
-    field  = "path-pattern"
-    values = ["${element(var.path_conditions, count.index)}"]
+    field = "path-pattern"
+    # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
+    # force an interpolation expression to be interpreted as a list by wrapping it
+    # in an extra set of list brackets. That form was supported for compatibility in
+    # v0.11, but is no longer supported in Terraform v0.12.
+    #
+    # If the expression in the following list itself returns a list, remove the
+    # brackets to avoid interpretation as a list of lists. If the expression
+    # returns a single list item then leave it as-is and remove this TODO comment.
+    values = [element(var.path_conditions, count.index)]
   }
 }
+
--- variables.tf
+++ variables.tf
@@ -1,24 +1,24 @@
 # required
 variable "alb_listener_arn" {
   description = "Listener to add the rule(s) to"
-  type        = "string"
+  type        = string
 }
 
 variable "target_group_arn" {
   description = "Target group to direct traffic to"
-  type        = "string"
+  type        = string
 }
 
 # optional
 variable "path_conditions" {
   description = "Defines path-based conditions for routing; separate by, eg. '/home,/home/*'"
-  type        = "list"
+  type        = list(string)
   default     = ["*"]
 }
 
 variable "host_condition" {
   description = "Defines host-based condition for rule (domain name)"
-  type        = "string"
+  type        = string
   default     = "*.*"
 }
 
@@ -26,3 +26,4 @@ variable "starting_priority" {
   description = "Starting priority for rules that will be added"
   default     = 50
 }
+
