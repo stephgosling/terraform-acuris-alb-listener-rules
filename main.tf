5c9379a0b9dd9b9ae45b07bfc85f75ccbcff4381
Steph Gosling <steph@chuci.org>
2020-11-06 13:58:43 +0000

    attemped fix for 0.12 syntax

--- main.tf
+++ main.tf
@@ -14,20 +14,12 @@ resource "aws_alb_listener_rule" "rule" {
   # NOTE: You can have multiple paths but only a single hostname
   condition {
     field  = "host-header"
-    values = [var.host_condition]
+    values = var.host_condition
   }
 
   condition {
-    field = "path-pattern"
-    # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
-    # force an interpolation expression to be interpreted as a list by wrapping it
-    # in an extra set of list brackets. That form was supported for compatibility in
-    # v0.11, but is no longer supported in Terraform v0.12.
-    #
-    # If the expression in the following list itself returns a list, remove the
-    # brackets to avoid interpretation as a list of lists. If the expression
-    # returns a single list item then leave it as-is and remove this TODO comment.
-    values = [element(var.path_conditions, count.index)]
+    field  = "path-pattern"
+    values = element(var.path_conditions, count.index)
   }
 }
 
